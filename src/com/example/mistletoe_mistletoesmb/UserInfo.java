e00615ab8b4a8ee0c2ad083610a8e874e147aeb1
StarsWhisper <554809247@qq.com>
2015-03-23 17:04:48 +0800

    新浪微博接口导入，认证授权，用户信息存取方法，新建数据库

---
+++ src/com/example/mistletoe_mistletoesmb/UserInfo.java
@@ -0,0 +1,57 @@
+package com.example.mistletoe_mistletoesmb;
+
+import android.graphics.drawable.Drawable;
+
+public class UserInfo {  //用户信息的存取方法
+	final static String MDID = "MistletoeDatabaseID";
+	final static String USERMBID = "UserMicroBlogID";
+	final static String USERMBNAME = "UserMicroBlogName";
+	final static String TOKEN = "TOKEN";
+	final static String TOKENSECRET = "TOKENSECRET";
+	final static String USERMBICON = "USERMicroBlogICON";
+	
+	private String mDId;
+	private String userMBId;
+	private String userMBName;
+	private String token;
+	private String tokenSecret;
+	private String userMBIcon;
+	
+	public void setMDId(String mDId){
+		this.mDId = mDId;
+	}
+	public void setUserMBId(String userMBId){
+		this.userMBId = userMBId;
+	}
+	public void setUserMBName(String userMBName){
+		this.userMBName = userMBName;
+	}
+	public void setToken(String token){
+		this.token = token;
+	}
+	public void setTokenSecret(String tokenSecret){
+		this.tokenSecret = tokenSecret;
+	}
+	public void setUserMBIcon(String userMBIcon){
+		this.userMBIcon = userMBIcon;
+	}
+	
+	public String getMDId(){
+		return mDId;
+	}
+	public String getUserMBId(){
+		return userMBId;
+	}
+	public String getUserMBName(){
+		return userMBName;
+	}
+	public String getToken(){
+		return token;
+	}
+	public String getTokenSecret(){
+		return tokenSecret;
+	}
+	public String getUserMBIcon(){
+		return userMBIcon;
+	}
+}
